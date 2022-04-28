37e3a93bc00184cdefae1469c036153c1429aaff
Nina Craven <nmc1313@gmail.com>
2022-04-27 20:40:25 -0400

    create live tracker

--- mapanimation.js
+++ mapanimation.js
@@ -14,23 +14,32 @@ const busStops = [
   [-71.118625, 42.374863],
 ];
 
-fetchBusLocations();
+var busLocations = new Array()
+var markerLocations = new Array()
 
 async function fetchBusLocations(){
-    var busLocations;
+    var location;
     const locations = await getBusLocations();
     console.log(new Date());
 
     for (let i=0; i < locations.length; i++){
-      console.log(locations[i]);
       location = locations[i].attributes;
-      busLocations[i] = [location.longitude, location.latitude];
+      if (busLocations[i] == null) 
+        busLocations.push([location.longitude, location.latitude]);
+      else 
+        busLocations[i] = [location.longitude, location.latitude];
+      if (markerLocations[i] == null) {
+        var marker = new mapboxgl.Marker();
+        markerLocations.push(marker);
+        marker.setLngLat(busLocations[i])
+          .addTo(map);
+      }
+      else {
+        markerLocations[i].setLngLat(busLocations[i]);
+      }
     }
 
-    console.log(busLocations);
-
-    setTimeout(fetchBusLocations, 10000);
-    //return locations;
+    setTimeout(fetchBusLocations, 2000);
 }
 
 async function getBusLocations(){
@@ -51,20 +60,22 @@ let map = new mapboxgl.Map({
 });
 
 // add a marker to the map at the first coordinates in the array busStops. The marker variable should be named "marker"
-var marker = new mapboxgl.Marker()
-  .setLngLat([-71.093729, 42.359244])
-  .addTo(map);
+// var marker = new mapboxgl.Marker()
+//   .setLngLat([-71.093729, 42.359244])
+//   .addTo(map);
 
 // counter here represents the index of the current bus stop
-let counter = 0;
-function move() {
-  // move the marker on the map every 1000ms. Use the function marker.setLngLat() to update the marker coordinates
-  // Use counter to access bus stops in the array busStops
-  // Make sure you call move() after you increment the counter.
-  setTimeout(()=>{
-      if (counter >= busStops.length) return;
-      marker.setLngLat(busStops[counter]);
-      counter ++; 
-      move();
-    }, 1000)
-}
+// let counter = 0;
+// function move() {
+//   // move the marker on the map every 1000ms. Use the function marker.setLngLat() to update the marker coordinates
+//   // Use counter to access bus stops in the array busStops
+//   // Make sure you call move() after you increment the counter.
+//   setTimeout(()=>{
+//       if (counter >= busStops.length) return;
+//       marker.setLngLat(busStops[counter]);
+//       counter ++; 
+//       move();
+//     }, 1000)
+// }
+
+fetchBusLocations();
